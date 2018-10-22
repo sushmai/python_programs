bfe55c0be437cffde25c3cf4a7d99fbc6aef77d6
sushmai <maryadareddy@gmail.com>
2018-10-21 20:50:13 -0700

    Add files via upload

---
+++ list_bind_twolists.py
@@ -0,0 +1,13 @@
+#program to combine to lists with out duplicate.
+
+a = [1,2,3,4,5]
+b = [5,4,5,6,7,1]
+print(set(a) & set(b))
+
+"""
+c = []
+d = []
+for i in a:
+    if i in b:
+        c.append()
+        
