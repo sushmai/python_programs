d970f31e611c3a72ca7156bf641f530a81cdbf91
sushmai <maryadareddy@gmail.com>
2018-10-26 22:00:53 -0700

    Update lambdafun.py

--- lambdafun.py
+++ lambdafun.py
@@ -12,6 +12,7 @@ my_list = [1,2,3,4,5,6,7]
 
 new_list = list(map(lambda x : x * 2, my_list))
 
+
 print (new_list)
 #combine first and last name to single "full name"
 full_name = lambda fn, ln: fn.strip().title() + " " + ln.strip().title()
